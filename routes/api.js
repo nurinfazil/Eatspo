fad609abe66dc83f6e96d734c7c57ae4bc979702
hamrajk <hamraj_khangura@hotmail.com>
2021-08-21 19:16:47 -0400

    Working on Backend API for saving posts and updating related values in users schema

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,54 @@ const Todo = require("../models/todo");
 const Users = require("../models/users");
 const Posts = require("../models/posts");
 
+//Given a userID and a postID, update the savedPosts field in users.js and the savedBy field in posts.js
+router.post("/savePostGivenUserAndPostIds", (req, res, next) => {
+  Users.update(
+    { userID: req.body.userID },
+    {
+      $set: {
+        savedPosts: req.body.postID,
+      },
+    }
+  )
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(next);
+
+  //   let tempObj = {};
+  //   Users.find({ userID: req.body.userID })
+  //     .then((data) => {
+  //       res.json(data);
+  //       console.log("first one, ", data[0]);
+  //       tempObj = data[0];
+  //       console.log("temp object ", tempObj);
+  //       const newUser = new Users({
+  //         _id: tempObj._id,
+  //         posts: tempObj.posts,
+  //         friends: tempObj.friends,
+  //         savedPosts: req.body.postID,
+  //         userID: tempObj.userID,
+  //         username: tempObj.username,
+  //         password: tempObj.password,
+  //         firstname: tempObj.firstname,
+  //         lastname: tempObj.lastname,
+  //         _v: tempObj._v,
+  //       });
+  //   console.log(newUser);
+
+  //   Users.updateOne({ userID: req.body.userID }, newUser)
+  //     .then((data) => {
+  //       res.json(data);
+  //     })
+  //     .catch(next);
+  //     })
+  //     .catch();
+
+  // TempObj.updateOne({ userID: req.body.userID }, { savedPosts: req.body.postID })
+});
+
+//This is to get Users info from user schema
 router.post("/findUserByUserName", (req, res, next) => {
   //this is to get the UserID
   //   let query = Users.find({ username: req.body.username });
@@ -16,8 +64,8 @@ router.post("/findUserByUserName", (req, res, next) => {
     .catch(next);
 });
 
+//This is to return all posts, given userID
 router.post("/findPostsGivenUserID", (req, res, next) => {
-  //this is to get the UserID
   //   let query = Users.find({ username: req.body.username });
   //   console.log(query);
   Posts.find({ userID: req.body.userID })
@@ -31,6 +79,18 @@ router.post("/findPostsGivenUserID", (req, res, next) => {
 router.post("/users", (req, res, next) => {
   if (req.body) {
     Users.create(req.body)
+      .then((data) => res.json(data))
+      .catch(next);
+  } else {
+    res.json({
+      error: "The input field is empty",
+    });
+  }
+});
+
+router.post("/posts", (req, res, next) => {
+  if (req.body) {
+    Posts.create(req.body)
       .then((data) => res.json(data))
       .catch(next);
   } else {
